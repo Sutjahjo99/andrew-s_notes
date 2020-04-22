3509c62b53bd870cfba69adc31d1f0e5368e33f1
Andrew <andrewowen1991@gmail.com>
2020-04-21 23:12:32 -0400

    Minor fixes

--- flaskblog/subjects/routes_math.py
+++ flaskblog/subjects/routes_math.py
@@ -9,5 +9,9 @@ def math_home():
     return render_template('/subjects/math/math_home.html', title="Math Home", math=True , subject_name="Mathematics")
 
 @math.route("/subjects/math/calc_i/intro")
-def math_calc_i_intro():
+def calc_i_intro():
     return render_template('subjects/math/calc_I/calc_I_intro.html', title="Calc I", topic=True, topic_name="Calculus I")
+
+@math.route("/subjects/math/calc_i/practice_problems")
+def calc_i_pp():
+    return render_template('subjects/math/calc_I/calc_I_pp.html', title="Calc I - Practice", topic=True, topic_name="Calculus I")
