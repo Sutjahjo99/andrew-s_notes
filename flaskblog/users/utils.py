338ece79e6d735d917303231f9b31319e94893d2
Andrew <andrewowen1991@gmail.com>
2020-06-01 19:43:50 -0400

    Added new CRE content

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -5,13 +5,23 @@ from flask import url_for, current_app
 from flask_mail import Message
 from flaskblog import mail
 
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
     
-    output_size = (125, 125)
+    output_size = (250, 250)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
